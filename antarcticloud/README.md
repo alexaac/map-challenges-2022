dd5e9a3419b9b93235f37c4159b2694406a0b808
Alexa Cristina <alexa@maptheclouds.com>
2022-11-29 20:37:40 +0200

    feat: Day 29 - Comfort Zone

---
+++ antarcticloud/README.md
@@ -0,0 +1,27 @@
+### Project
+
+#30DayMapChallenge 2022 Day 29. Out of My Comfort Zone
+
+### Title
+
+Antarctic Cloud ☁️
+
+### Demo
+
+[https://maptheclouds.com/playground/30-day-map-challenge-2022/antarcticloud/](https://maptheclouds.com/playground/30-day-map-challenge-2022/antarcticloud/)
+
+### Description
+
+200m elevation.
+
+Liu, H., K. C. Jezek, B. Li, and Z. Zhao. 2015. Radarsat Antarctic Mapping Project Digital Elevation Model, Version 2. [Indicate subset used]. Boulder, Colorado USA. NASA National Snow and Ice Data Center Distributed Active Archive Center. doi: http://dx.doi.org/10.5067/8JKNEW6BFRVD. [29-11-2022].
+
+### Data sources
+
+[Radarsat Antarctic Mapping Project Digital Elevation Model Version 2](https://www.npolar.no/quantarctica/#toggle-id-20)
+
+### Inspiration
+
+### TODO
+
+TODO
---
+++ antarcticloud/js/main.js
@@ -0,0 +1,161 @@
+import * as THREE from '../../js/three/build/three.module.js';
+import { FlyControls } from '../../js/three/FlyControls.js';
+import * as dat from '../../js/libs/lil-gui.module.min.js';
+
+const isMobile = window.innerWidth < 703;
+
+/**
+ * Loaders
+ */
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+const textureLoader = new THREE.TextureLoader();
+
+/**
+ * Textures
+ */
+
+const texture = textureLoader.load('./assets/textures/antarcticloud.png');
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI({ closed: true });
+const parameters = {
+  displacementScale: 0,
+};
+gui.hide();
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Objects
+ */
+
+const geometry = new THREE.PlaneBufferGeometry(3, 1.96, 32, 32);
+
+// Meshes
+const objectsDistance = 2;
+let index = 0;
+let xOffset = 0;
+const sectionMeshes = [];
+
+const div = document.querySelector('.list');
+
+/**
+ * @description Fetch data
+ * @param {string} url - file
+ */
+const getData = async (url) => {
+  const response = fetch(url);
+
+  const data = await (await response).json();
+
+  return data;
+};
+
+const material = new THREE.MeshBasicMaterial({
+  map: texture,
+  color: 0xffffff,
+  side: THREE.DoubleSide,
+  transparent: true,
+  depthTest: false,
+});
+
+// Mesh
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+/**
+ * Lights
+ */
+
+var ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1);
+directionalLight.position.set(1, 1, 0);
+scene.add(directionalLight);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(0, 0, 1.3);
+
+scene.add(camera);
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  alpha: true,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Controls
+ *
+ */
+
+const controls = new FlyControls(camera, canvas);
+controls.movementSpeed = 0.1;
+controls.domElement = renderer.domElement;
+controls.rollSpeed = Math.PI / 24;
+controls.autoForward = false;
+controls.dragToLook = false;
+
+const clock = new THREE.Clock();
+
+/**
+ * Animate
+ */
+
+const tick = () => {
+  // Render
+  renderer.render(scene, camera);
+
+  const delta = clock.getDelta();
+  controls.update(delta);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
