edf53463074a717ac4f7682a5aa2a22f5d51fe9a
Alexa Cristina <alexa@maptheclouds.com>
2022-12-09 14:01:21 +0200

    refactor: add hide, add photos, add star extra

--- js/hide.js
+++ js/hide.js
@@ -1,23 +1,23 @@
 window.addEventListener('keydown', (event) => {
   if (event.key === 'h') {
     if (document.querySelector('.nav--gridContainer'))
-      document.querySelector('.nav--gridContainer').classList.add('hidden');
+      document.querySelector('.nav--gridContainer').classList.add('hide-all');
 
     if (document.querySelector('.side-pannel'))
-      document.querySelector('.side-pannel').classList.add('hidden');
+      document.querySelector('.side-pannel').classList.add('hide-all');
 
     if (document.querySelector('.main--contentWrapper'))
-      document.querySelector('.main--contentWrapper').classList.add('hidden');
+      document.querySelector('.main--contentWrapper').classList.add('hide-all');
 
     if (document.querySelector('.content--contentWrapper'))
       document
         .querySelector('.content--contentWrapper')
-        .classList.add('hidden');
+        .classList.add('hide-all');
 
     if (document.querySelector('.content--subCopy'))
-      document.querySelector('.content--subCopy').classList.add('hidden');
+      document.querySelector('.content--subCopy').classList.add('hide-all');
 
     if (document.querySelector('.below'))
-      document.querySelector('.below').classList.add('hidden');
+      document.querySelector('.below').classList.add('hide-all');
   }
 });
